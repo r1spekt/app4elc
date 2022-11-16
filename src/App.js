3c96b9212f77c1a2643faed9196739a700b316c5
r1spekt <r1spekt93@gmail.com>
2022-11-16 09:53:26 +0300

    initial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+const tg = window.Telegram.WebApp;
+function App() {
+  const onClose = () =>{
+    tg.close()
+  }
+  return (
+    <div className="App">
+        work
+    </div>
+  );
+}
+
+export default App;
