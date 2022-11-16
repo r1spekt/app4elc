0d0ba21d7f4caa43f23308c6aad15b89ae8e5d09
r1spekt <r1spekt93@gmail.com>
2022-11-16 15:52:02 +0300

    initial 5.0

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import './App.css';
-const tg = window.Telegram.WebApp;
 function App() {
   return (
     <div className="App">
-        work
-        <form>
-            <input>
-
-            </input>
-        </form>
     </div>
   );
 }
