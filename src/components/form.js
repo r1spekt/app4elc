97a9b85c28fa9fa96132f00952a0135cce7e47bc
r1spekt <r1spekt93@gmail.com>
2022-11-16 12:45:34 +0300

    initial 2.0

---
+++ src/components/form.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class From extends React.Component {
+    render() {
+        return (
+            <form>
+                aaa
+            </form>
+        );
+    }
+}
+
+export default Form;
